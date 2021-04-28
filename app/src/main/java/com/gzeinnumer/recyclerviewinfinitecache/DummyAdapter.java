ae22b832d7a7aac7b495419df31edecbcc01e7d4
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-04-28 15:56:50 +0700

    Initial commit

---
+++ app/src/main/java/com/gzeinnumer/recyclerviewinfinitecache/DummyAdapter.java
@@ -0,0 +1,64 @@
+package com.gzeinnumer.recyclerviewinfinitecache;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.gzeinnumer.recyclerviewinfinitecache.databinding.RvItemBinding;
+
+import java.util.ArrayList;
+
+public class DummyAdapter extends RecyclerView.Adapter<DummyAdapter.MyHolder> {
+
+    private ArrayList<String> list;
+    private ArrayList<DummyAdapter.MyHolder> holders;
+
+    public DummyAdapter(ArrayList<String> list) {
+        this.list = new ArrayList<>(list);
+        this.holders = new ArrayList<>(list.size());
+        initHolders();
+    }
+
+    public void setList(ArrayList<String> list) {
+        this.list = list;
+        initHolders();
+    }
+
+    private void initHolders() {
+        for (int i = 0; i < list.size(); i++) {
+            holders.add(null);
+        }
+    }
+
+    public ArrayList<MyHolder> getHolders() {
+        return holders;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+        holders.set(position, holder);
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyHolder(RvItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class MyHolder extends RecyclerView.ViewHolder {
+        public RvItemBinding binding;
+
+        public MyHolder(@NonNull RvItemBinding itemView) {
+            super(itemView.getRoot());
+            binding = itemView;
+        }
+    }
+}
---
+++ app/src/main/java/com/gzeinnumer/recyclerviewinfinitecache/MainActivity.java
@@ -0,0 +1,51 @@
+package com.gzeinnumer.recyclerviewinfinitecache;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.gzeinnumer.recyclerviewinfinitecache.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private DummyAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        iniRV();
+    }
+
+    private void iniRV() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(String.valueOf(i+1));
+        }
+        adapter = new DummyAdapter(list);
+        binding.rv.setAdapter(adapter);
+        binding.rv.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        binding.rv.hasFixedSize();
+//        binding.rv.setItemViewCacheSize(100);
+        binding.rv.setItemViewCacheSize(list.size());
+
+        getItemPerIndex();
+    }
+
+    private void getItemPerIndex() {
+        for (int i = 0; i < adapter.getHolders().size(); i++) {
+            DummyAdapter.MyHolder holder = adapter.getHolders().get(i);
+
+            String data = holder.binding.tv.getText().toString();
+            Log.d(getClass().getSimpleName(), "getItemPerIndex: "+data);
+
+        }
+    }
+}
